4bbdd375b2a7708973b7bb84f87c380c217da7f3
Beatrice Lee <beatricelee@Beatrices-MacBook-Pro.local>
2020-10-07 16:25:16 +0900

    business_weekday_plot fix

---
+++ business_weekday_plot.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import datetime
+
+input_json_checkin = "../yelp_dataset/yelp_academic_dataset_checkin.json"
+businessid= "--9e1ONYQuAa-CB_Rrw7Tw"
+
+def load(input_json):
+    return pd.read_json(input_json, lines = True)
+
+def dateParser(df):
+    df_parsed = pd.DataFrame(checkin.loc[df['business_id']==businessid].date.iloc[0].split(", "), columns=["date"])
+    df_parsed["date"] = pd.to_datetime(df_parsed["date"])
+
+    df_parsed["weekday"] = df_parsed["date"].dt.day_name()
+    df_parsed["hour"] = df_parsed["date"].dt.hour
+
+    return df_parsed
+
+def getCheckinByHour(df, weekday, businessid):
+    return df[df["weekday"]==weekday].groupby("hour").count()["date"]
+
+
+checkin = load(input_json_checkin)
+checkin_parsed = dateParser(checkin)
+
+getCheckinByHour(checkin_parsed, "Monday", businessid)
+
